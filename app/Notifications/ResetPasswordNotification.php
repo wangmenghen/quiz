d7a570f19514f0fc188b9465e7c8e7fa72bb89f5
wangmenghen <734452449@qq.com>
2018-02-13 00:11:27 +0800

    覆盖auth自带的密码重置模板

---
+++ app/Notifications/ResetPasswordNotification.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class ResetPasswordNotification extends Notification
+{
+    use Queueable;
+
+    public $token;
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    // ->line('The introduction to the notification.')
+                    // ->action('Notification Action', 'https://laravel.com')
+                    // ->line('Thank you for using our application!');
+                    // ->subject('XXXX')
+                    // ->salutation('XXX')
+                    ->line('您之所以收到这封邮件是因为我们收到了您重置密码的申请。')
+                    ->action('Reset Password', url(config('app.url').route('auth.password.reset', $this->token, false)))
+                    ->line('如果您本人未进行密码重置，您可以不必采取进一步操作!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
